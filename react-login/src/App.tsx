c4537b9159c0e8546884e4fd3d0c7d39a64708b5
Tamires Cavalcante <tmires.cavalcante@gmail.com>
2022-02-19 19:55:04 -0300

    incompleted login

---
+++ react-login/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import './App.css';
+import { Home } from './pages/Home';
+import { Private } from './pages/Private';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Header do site</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/private">Private Page</Link>
+        </nav>
+      </header>
+      <hr />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/private" element={<Private />} />
+        
+      </Routes>
+
+    </div>
+  );
+}
+
+export default App;
