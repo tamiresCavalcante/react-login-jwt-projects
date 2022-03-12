b8ddf0e09781993c75d560c1353e3d5987eb0be7
Tamires Cavalcante <tmires.cavalcante@gmail.com>
2022-03-12 13:52:48 -0300

    crud com  autenticação

--- authentication-jwt-react-node/server/src/routes.js
+++ authentication-jwt-react-node/server/src/routes.js
@@ -1,18 +1,28 @@
 import { Router } from "express";
+import auth from "./middleware/auth";
 import HelloController from "./controllers/HelloController";
+import SessionController from "./controllers/SessionController";
 import UsersController from "./controllers/UsersController";
+import RepositoriesController from "./controllers/RepositoriesController";
 
 const routes = new Router();
 
+routes.use(auth);
+
+routes.post('/sessions', SessionController.create);
 routes.get('/hello', HelloController.index);
 
 // RESTFull
 
 routes.get('/users', UsersController.index);
-routes.get('/users:id', UsersController.show);
+routes.get('/users/:id', UsersController.show);
 routes.post('/users', UsersController.create);
-routes.put('/users:id', UsersController.update);
-routes.delete('/users:id', UsersController.destroy);
+routes.put('/users/:id', UsersController.update);
+routes.delete('/users/:id', UsersController.destroy);
 
+
+routes.get('/users/:user_id/repositories', RepositoriesController.index);
+routes.post('/users/:user_id/repositories', RepositoriesController.create);
+routes.delete('/users/:user_id/repositories', RepositoriesController.destroy);
 
 export default routes;
